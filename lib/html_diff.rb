51ef2343ec2bcdbd8aa65df692efd494dd418d1d
Sarah Adams <sadams.codes@gmail.com>
2013-10-20 00:46:11 -0700

    better handling of empty diff

--- lib/html_diff.rb
+++ lib/html_diff.rb
@@ -8,7 +8,7 @@ class HtmlDiff
 
   def stats_line
     stats=`git diff --stat #{@argv.join(" ")}`
-    if stats.blank?
+    if stats == ""
       return "no changes"
     end
     stats.scan(/\d+ files? changed, \d+ insertions?\(\+\), \d+ deletions?\(\-\)/).last
@@ -21,9 +21,6 @@ class HtmlDiff
 
   def html_diff
     diff=`git diff #{@argv.join(" ")}`
-    if diff.blank?
-      return
-    end
     diff.gsub!(/^diff \-\-git a\/([^\s]*).*\n.*$/) { "<div class='diff-file'>"+$1+"</div>" }
     diff.gsub!(/^(\-\-\-\s)(a.*)$/) { "<div class='code'><span class='delete'>"+$1+"&nbsp;"+$2+"</span>" }
     diff.gsub!(/(\n\<div class\=\'diff-file\'\>)/) { "</div>"+$1}
